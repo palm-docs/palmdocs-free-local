73069add6a1841a19b05a3c7772bfabcc4e858f3
maonianyou <maonianyou@gmail.com>
2025-04-25 15:42:23 +0800

    build: use vite-plugin-compression

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'node:url'
 import { dirname, resolve } from 'node:path'
+import viteCompression from 'vite-plugin-compression'
 
 // 在 ESM 中获取 __dirname 的替代方案
 const __filename = fileURLToPath(import.meta.url)
@@ -20,6 +21,13 @@ export default defineConfig({
   },
   plugins: [
     tailwindcss(),
-    vue()
+    vue(),
+    viteCompression({
+      verbose: true,  // 显示压缩日志
+      disable: false, // 是否禁用
+      threshold: 102400, // 大于 100KB 的文件才压缩
+      algorithm: 'gzip', // 压缩算法
+      ext: '.gz', // 文件扩展名
+    }),
   ],
 })
